6208c71ac4e1c72a231dac18c2708292f5a399a9
Josiah <josiah.s.murray.work@gmail.com>
2025-10-03 09:34:40 +1000

    Update variable names and otherwise cleaned GWR function

--- GaverWynnRho.jl
+++ GaverWynnRho.jl
@@ -3,88 +3,79 @@ module GaverWynnRho
 using SpecialFunctions
 
 
-#TODO: Rewrite to make more consistent with standard notation.
+#TODO: Add comment and citation to InverseLaplace.jl
+#TODO: Is it compatible with the ArbNumerics?
 function GWR(func, t, M)
-    Dt = typeof(t)
-    bM = convert(Dt, M) #Unused
 
+  Dt = typeof(t)
 
-
-
-    #||--Gaver functionals--||#
-    tau = log(convert(Dt, 2)) / t
-    broken = false #used in case of division by zero in wynn rho
-    Fi = Array{Dt}(undef, 2 * M)
+  #||--Gaver functionals--||#
+  tau = log(convert(Dt, 2)) / t #Sample points of Laplace domain function, `func` for Gaver functionals.
+  Fi = Array{Dt}(undef, 2 * M) #Stores evaluations of `func`
   @inbounds  for i in 1: 2 * M #The @inbounds improves performance by preventing an internal check. Will over right other data if implemented wrong.
-        Fi[i] = func(i * tau) #Necessary function values are pre-calculated as many are re-used
+    Fi[i] = func(i * tau) #Necessary function values are pre-calculated as many are re-used
+  end
+
+  ρ_0 = zeros(Dt, M + 1)#stores Gaver functionals 0 through M which are all needed for Wynn rho. Later over-written as part of the Wynn rho algorithm.
+
+  #Calculate Gaver functionals
+  for n in 1:M
+    sm = zero(Dt)
+    bn = big(n)
+    for i in 0:n
+      sm += binomial(bn, big(i)) * (-1)^i * Fi[n + i]
     end
-    M1 = M
-    G0 = zeros(Dt, M1 + 1)#stores Gaver functionals 0 through M which are all needed for Wynn rho
+    #The factor bn (k in math) out the front cancels with one of the factorials
+    ρ_0[n+1] = tau * SpecialFunctions.factorial(2 * bn) /
+      (SpecialFunctions.factorial(bn) * SpecialFunctions.factorial(bn - 1)) * sm
+  end
 
-    #Calculate Gaver functionals
-    for n in 1:M
-        sm = zero(Dt)
-        bn = big(n)
-        for i in 0:n
-            bi = convert(Dt, i)
-            sm += binomial(big(n), big(i)) * (-1)^i * Fi[n + i]
-        end
-        #The factor bn (k in math) out the front cancels with one of the factorials
-        G0[n+1] = tau * SpecialFunctions.factorial(2 * bn) /
-            (SpecialFunctions.factorial(bn) * SpecialFunctions.factorial(bn - 1)) * sm
+
+  #||--Wynn rho--||#
+
+
+  broken = false #used in case of division by zero in wynn rho
+  #Wynn rho uses values at 2 previous steps
+  ρ_m = zeros(Dt, M + 1)#Stores the ρ values for ρ_{i-1}^( ... )
+  ρ_p = zeros(Dt, M + 1)#Stores the newly calculated ρ values for ρ_{i+1}^( ... )
+  #ρ_{i}^( ... ) are stored in `ρ_0`
+
+
+  best_approximation = ρ_0[M] #Stores last valid approximation in case of division by zero. This can happen when the method recovers the exact solution.
+  for k in 1:M
+
+    for n in (M - k):-1:0
+      denominator = ρ_0[n + 2] - ρ_0[n + 1]#Denominator part of recursive step in Wynn rho. Check if zero before proceeding.
+      #Check if there will be division by zero. If yes, stop algorithm.
+      if denominator == 0
+        broken = true
+        @warn "Division by zero pre-empted on Wynn rho step $k out of $(M-2).\n    Laplace function: $func"
+        break
+      end
+      #Calculate next terms in Wynn rho
+      ρ_p[n + 1] = ρ_m[n + 2] + (k) / denominator
+
+      #Check if valid approximation
+      if iseven(k) && n == M - k
+        best = expr
+      end
     end
 
 
-    #BUG: G0[end] is always equal to zero for some reason.
-    #I think G0[n] should actually be G0[n+1].
-    ##TODO: Don't forget to take out the @inbounds while testing
-    #print(G0[end])
-
-
-
-    #||--Wynn rho--||#
-
-    #Wynn rho uses values at 2 previous steps
-    Gm = zeros(Dt, M1 + 1)
-    Gp = zeros(Dt, M1 + 1)
-
-
-    best = G0[M1] #Stores approximation at previous step in case of division by zero error.
-    for k in 1:M1
-        for n in (M1 - k):-1:0
-            expr = G0[n + 2] - G0[n + 1]#Denominator part of recursive step in Wynn rho
-            #Check if there will be division by zero. If yes, stop algorithm.
-            if expr == 0
-                broken = true
-                @warn "Division by zero pre-empted on Wynn rho step $k out of $(M1-2).\n    Laplace function: $func"
-                break
-            end
-            #Calculate next terms in Wynn rho
-            expr = Gm[n + 2] + (k) / expr
-            Gp[n + 1] = expr
-
-            #This true?
-            if iseven(k) && n == M1 - k
-                best = expr
-            end
-        end
-        if broken break end
-        #Move to next rows of approximations
-        for n in 0:(M1-k)
-            Gm[n + 1] = G0[n + 1]
-            G0[n + 1] = Gp[n + 1]
-
-            #Gm = copy(G0)
-            #G0 = copy(Gp)
-
-        end
+    if broken
+      break
     end
-    best
+
+    #Move to next rows of approximations
+    for n in 0:(M-k)
+      ρ_m[n + 1] = ρ_0[n + 1]
+      ρ_0[n + 1] = ρ_p[n + 1]
+    end
+  end
+  best_approximation
 end
 
-
-
-#TODO: Implement.
+#TODO: Consider moving to a different file.
 #func_array(s) should return an array of values (possible multiple dimensions).
 function GWR_array(func_array, t, M)
     Dt = typeof(t)
