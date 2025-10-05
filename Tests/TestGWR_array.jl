ff9a4640fdec906c7ad865aec8d0b262dd2f90f0
Josiah <josiah.s.murray.work@gmail.com>
2025-10-05 15:48:59 +1100

    Updated GWR_array test

--- Tests/TestGWR_array.jl
+++ Tests/TestGWR_array.jl
@@ -1,11 +1,16 @@
+using Revise
+include(joinpath(@__DIR__,"../NILaplace.jl"))
 using .NILaplace: GWR_array
 using .NILaplace: GWR
 
+t = 2.0
+
+
 TestFuncArray = s -> [[NILaplace.TestFunctions.Cohen1(s), NILaplace.TestFunctions.Cohen2(s)];; [NILaplace.TestFunctions.Cohen3(s), NILaplace.TestFunctions.Cohen4(s)]]
+exact = [[NILaplace.TestFunctions.Cohen1_exact(t), NILaplace.TestFunctions.Cohen2_exact(t)];;  [NILaplace.TestFunctions.Cohen3_exact(t), NILaplace.TestFunctions.Cohen4_exact(t) ]]
 
 
 
-
-multDimAnswer = GWR_array(TestFuncArray, 1.0, 20)
+multDimAnswer = GWR_array(TestFuncArray, t, 20)
 println("Multidimensional answer: ", multDimAnswer)
 GWR(NILaplace.TestFunctions.Cohen1, 1.0, 20)
